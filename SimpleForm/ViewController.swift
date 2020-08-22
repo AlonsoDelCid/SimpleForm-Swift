17526754772dd7401a98818449e213baa1736754
Alonso Del Cid <yosoy@alonsodelcid.com>
2020-08-22 12:50:16 -0700

    Initial Commit

---
+++ SimpleForm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleForm
+//
+//  Created by Alonso Del Cid on 8/22/20.
+//  Copyright © 2020 Alonso Del Cid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
